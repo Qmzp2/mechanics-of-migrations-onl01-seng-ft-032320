9fc13ec78d8bd519ac15535b46102c5bf225be59
Kevin Mathias <komathias1@gmail.com>
2020-05-02 20:50:30 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -5,7 +5,9 @@ class CreateArtists < ActiveRecord::Migration[5.2] #migration code, in this case
   def down #code when roll back migration
   end
   
-  def change #used for basic migrations. Primary way, works for majority of cases where AR knows how to reverse the migration automatically
+  def change                     #used for basic migrations. Primary way, works for majority of cases where AR knows how to reverse the migration automatically
+  create_table :artists do |t|   #here we added the create_table method and passed the name of teh table we want to create as a symbol.
+   end                           #other options include: remove_table, rename_table, remove_column, add_column and more
   end
   
 end
